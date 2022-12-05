db23e078a9b6055dc9d21b3252991f74fa2fecc7
Chris <christopher.lopez.09@gmail.com>
2022-12-05 17:18:11 -0500

    order schema

---
+++ models/order.js
@@ -0,0 +1,30 @@
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+// singular name because all orders will use this single order formula
+// ODM non relational DB
+
+const OrderSchema = Schema({
+    items: {
+        type: String,
+        required: true,
+
+    },
+    isComplete: {
+        type: Boolean,
+        default: false,
+    },
+    name: {
+        type: String,
+        required: true,
+    },
+    address:{
+        type: String,
+        required: true,
+    }}, {timestamps: true});
+// this makes things explicit and clean instead of the calling the mongoose.Schema directly
+// we want timestamps to see when the order came in
+
+
+export const Order = mongoose.model("Order", OrderSchema);
+//we are gonna create a new model using the schema and export the saved model
